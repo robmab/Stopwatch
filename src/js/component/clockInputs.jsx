2cc657024a964c33d44cd0f912896065eff00a73
robmab <rob_mb@outlook.es>
2023-04-14 21:12:55 +0200

    ALERT AND RESET, GREEN COLOR

--- src/js/component/clockInputs.jsx
+++ src/js/component/clockInputs.jsx
@@ -1,33 +1,100 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { regTime } from "../index.js";
+import { regTime, play, pause, reset, alertClick } from "../index.js";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faPause,
+  faRotateRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 //create your first component
 export const ClockInputs = (props) => {
+  console.log(props.color);
   return (
-    <div className="input-group mb-3">
-      <input
-        type="number"
-        className="form-control"
-        placeholder={props.placeholder}
-        aria-label=""
-        aria-describedby="basic-addon1"
-        min={0}
-        max={999999}
-      />
-      <div className="input-group-prepend">
-        <button
-          onClick={regTime}
-          className="btn btn-outline-secondary"
-          type="button"
-        >
-          Cuenta regresiva (seg)
-        </button>
+    <div className="inputs container">
+      <div className="row">
+        {/* CUENTA REGRESIVA */}
+        <div className="cuentaRegresiva col-12 col-lg-5">
+          <div className="input-group mb-5 ">
+            <input
+              type="number"
+              className="form-control"
+              placeholder=""
+              aria-label=""
+              aria-describedby="basic-addon1"
+              min={0}
+              max={999999}
+            />
+            <div className="input-group-prepend">
+              <button
+                onClick={regTime}
+                className="btn btn-outline-secondary"
+                type="button"
+              >
+                Cuenta regresiva (s)
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* CUENTA REGRESIVA END*/}
+        {/* BUTTONS */}
+        <div className="buttons col-12 col-lg-2">
+          <button
+            onClick={play}
+            type="button"
+            className="btn btn-outline-dark "
+          >
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+          <button
+            onClick={pause}
+            type="button"
+            className="btn btn-outline-dark"
+          >
+            <FontAwesomeIcon icon={faPause} />
+          </button>
+          <button
+            onClick={reset}
+            type="button"
+            className="btn btn-outline-dark"
+          >
+            <FontAwesomeIcon icon={faRotateRight} />
+          </button>
+        </div>
+        {/* BUTTONS END*/}
+        {/* ALERT */}
+        <div className=" alertClock col-12 col-lg-5">
+          <div  className="input-group mb-5 ">
+            <div className="input-group-prepend">
+              <button
+                onClick={alertClick}
+                style={{backgroundColor:props.color}}
+                className="btn btn-outline-secondary"
+                type="button"
+              >
+                Alerta (s)
+              </button>
+            </div>
+            <input
+              type="number"
+              className="form-control"
+              placeholder=""
+              aria-label=""
+              aria-describedby="basic-addon1"
+              min={0}
+              max={999999}
+            />
+          </div>
+        </div>
+        {/* ALERT END*/}
       </div>
     </div>
   );
 };
+
 ClockInputs.propTypes = {
-  placeholder: PropTypes.string,
+  color:PropTypes.string
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,43 +8,21 @@ import "../styles/index.css";
 //import your own components
 import { Clock } from "./component/clock.jsx";
 
-//render your react application
-
+//Variables
+let paused = false;
 let time = 0;
+let inputAlert = -1;
+let color = "";
+let clockOn = true;
+
+//Loop setInterval
 const timeRender = () => {
-  ReactDOM.render(
-    <Clock
-      clockOne={time}
-      clockTwo={Math.floor(time / 10)}
-      clockThree={Math.floor(time / 100)}
-      clockFour={Math.floor(time / 1000)}
-      clockFive={Math.floor(time / 10000)}
-      clockSix={Math.floor(time / 100000)}
-      placeholder="0"
-    />,
-    document.querySelector("#app")
-  );
-  time++;
-};
-
-timeRender(); //First time render
-
-let interval = setInterval(timeRender, 1000); //Loop 
-
-/* CUENTA REGRESIVA */
-
-let negativeInt;
-export const regTime = (e) => {
-  let input = e.target.parentNode.parentNode.childNodes[0];
-
-  clearInterval(interval);
-  clearInterval(negativeInt);
-
-  time = Number(input.value);
-  let placeholderTime=Number(input.value)
-  if (time<0) return ;  // dont let negative input
-
-  const timeRenderNegative = () => {
+  clockOn=true;
+  if (!paused) {
+    if (inputAlert == time) {
+      color = "";
+      alert(`Alcanzaste el segundo ${time}!`);
+    }
     ReactDOM.render(
       <Clock
         clockOne={time}
@@ -53,16 +31,86 @@ export const regTime = (e) => {
         clockFour={Math.floor(time / 1000)}
         clockFive={Math.floor(time / 10000)}
         clockSix={Math.floor(time / 100000)}
-        placeholder={placeholderTime}
+        color={color}
       />,
       document.querySelector("#app")
     );
-    if (time === 0) clearInterval(negativeInt);
-    time--;
+    time++;
+  }
+};
+
+timeRender(); //First time render
+
+let interval = setInterval(timeRender, 1000); //Loop
+
+/* CUENTA REGRESIVA */
+
+let negativeInt;
+export const regTime = (e) => {
+  clearInterval(interval);
+  clearInterval(negativeInt);
+
+  let input = e.target.parentNode.parentNode.childNodes[0];
+
+  time = Number(input.value);
+
+  if (time <= 0) return timeRender(); // dont let negative input
+
+  //Loop setInterval
+  const timeRenderNegative = () => {
+    clockOn=false;
+
+    if (!paused) {
+      if (inputAlert == time) {
+        color = "";
+        alert(`Alcanzaste el segundo ${time}!`);
+      }
+      ReactDOM.render(
+        <Clock
+          clockOne={time}
+          clockTwo={Math.floor(time / 10)}
+          clockThree={Math.floor(time / 100)}
+          clockFour={Math.floor(time / 1000)}
+          clockFive={Math.floor(time / 10000)}
+          clockSix={Math.floor(time / 100000)}
+          color={color}
+        />,
+        document.querySelector("#app")
+      );
+      if (time === 0) clearInterval(negativeInt);
+      time--;
+    }
   };
 
   timeRenderNegative(); //First time render
   negativeInt = setInterval(timeRenderNegative, 1000); //Loop render
+};
 
-  if (time < 0) clearInterval(negativeInt);
+/* PLAY-PAUSED-RESET */
+export const play = () => {
+  paused = false;
+};
+
+export const pause = () => {
+  paused = true;
+};
+
+export const reset = () => {
+  time = 0;
+  timeRender();
+  if (paused === true) paused = false;
+};
+
+/* ALERT */
+
+export const alertClick = (e) => {
+  inputAlert = e.target.parentNode.parentNode.childNodes[1].value;
+  color = "rgb(201, 252, 126)";
+
+  //first render
+  if (clockOn){
+    timeRender()
+  }else{
+    timeRenderNegative()
+  }
 };
